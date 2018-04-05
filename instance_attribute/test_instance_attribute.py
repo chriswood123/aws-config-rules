eab325e759496ecd464e550773071ca84d4c34fd
Chris Wood <chris.wood@cloudreach.com>
2018-04-05 17:58:18 +0100

    Convert tests from pytest to unittest

--- instance_attribute/test_instance_attribute.py
+++ instance_attribute/test_instance_attribute.py
@@ -1,78 +1,70 @@
+import unittest
 import boto3
 from moto import mock_ec2
 from instance_attribute import evaluate_compliance
 
 
-def test_not_ec2_instance():
-    assert evaluate_compliance(
-        {'resourceType': 'notEC2'},
-        'i-11',
-        'a',
-        'b'
-    ) == 'NOT_APPLICABLE'
-
-
 @mock_ec2
-def test_is_compliant():
-    mock_instance = boto3.client('ec2').run_instances(
-        MaxCount=1,
-        MinCount=1,
-        InstanceType='t2.small'
-    )
-    instance_id = mock_instance['Instances'][0]['InstanceId']
+class TestEvaluateCompliance(unittest.TestCase):
 
-    assert evaluate_compliance(
-        {'resourceType': 'AWS::EC2::Instance'},
-        instance_id,
-        'InstanceType',
-        't2.small'
-    ) == 'COMPLIANT'
+    def setUp(self):
+        self.client = boto3.client('ec2')
+        self.image_id = self.client.describe_images()['Images'][0]['ImageId']
+        self.mock_instance = self.client.run_instances(
+            MaxCount=1,
+            MinCount=1,
+            ImageId=self.image_id,
+            InstanceType='t2.small'
+        )
+        self.instance_id = self.mock_instance['Instances'][0]['InstanceId']
+
+    def test_not_ec2_instance(self):
+        result = evaluate_compliance(
+            {'resourceType': 'notEC2'},
+            'i-11',
+            'a',
+            'b'
+        )
+        self.assertEqual(result, 'NOT_APPLICABLE')
+
+    def test_is_compliant(self):
+        result = evaluate_compliance(
+            {'resourceType': 'AWS::EC2::Instance'},
+            self.instance_id,
+            'InstanceType',
+            't2.small'
+        )
+        self.assertEqual(result, 'COMPLIANT')
+
+    def test_is_non_compliant(self):
+        result = evaluate_compliance(
+            {'resourceType': 'AWS::EC2::Instance'},
+            self.instance_id,
+            'InstanceType',
+            'm3.medium'
+        )
+        self.assertEqual(result, 'NON_COMPLIANT')
+
+    def test_attribute_not_found(self):
+        result = evaluate_compliance(
+            {'resourceType': 'AWS::EC2::Instance'},
+            self.instance_id,
+            'FakeKey',
+            'FakeValue'
+        )
+        self.assertEqual(result, 'NON_COMPLIANT')
+
+    def test_json_attribute_value(self):
+        result = evaluate_compliance(
+            {'resourceType': 'AWS::EC2::Instance'},
+            self.instance_id,
+            'Monitoring',
+            # Moto always returns monitoring disabled
+            # when describing an instance
+            {'State': 'disabled'}
+        )
+        self.assertEqual(result, 'COMPLIANT')
 
 
-@mock_ec2
-def test_is_non_compliant():
-    mock_instance = boto3.client('ec2').run_instances(
-        MaxCount=1,
-        MinCount=1,
-        InstanceType='t2.small'
-    )
-    instance_id = mock_instance['Instances'][0]['InstanceId']
-
-    assert evaluate_compliance(
-        {'resourceType': 'AWS::EC2::Instance'},
-        instance_id,
-        'InstanceType',
-        'm3.medium'
-    ) == 'NON_COMPLIANT'
-
-
-@mock_ec2
-def test_attribute_not_found():
-    mock_instance = boto3.client('ec2').run_instances(
-        MaxCount=1,
-        MinCount=1,
-        InstanceType='t2.small'
-    )
-    instance_id = mock_instance['Instances'][0]['InstanceId']
-
-    assert evaluate_compliance(
-        {'resourceType': 'AWS::EC2::Instance'},
-        instance_id,
-        'FakeKey',
-        'FakeValue'
-    ) == 'NON_COMPLIANT'
-
-
-@mock_ec2
-def test_json_attribute_value():
-    mock_instance = boto3.client('ec2').run_instances(MaxCount=1, MinCount=1)
-    instance_id = mock_instance['Instances'][0]['InstanceId']
-
-    assert evaluate_compliance(
-        {'resourceType': 'AWS::EC2::Instance'},
-        instance_id,
-        'Monitoring',
-        # Moto always returns monitoring disabled
-        # when describing an instance
-        {'State': 'disabled'}
-    ) == 'COMPLIANT'
+if __name__ == '__main__':
+    unittest.main()
